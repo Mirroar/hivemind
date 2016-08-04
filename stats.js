952917f4c934cf2eb94b0534d3d98a0f4ca181b0
David Franke <mirroar@gmail.com>
2016-08-04 09:29:39 +0200

    added ability to clear harvest revenue stats

--- stats.js
+++ stats.js
@@ -16,10 +16,22 @@ var stats = {
             };
         }
 
-        // @toto Temporary.
+        // @todo Temporary.
         if (!memory.remoteHarvesting[target].defenseCost) {
             memory.remoteHarvesting[target].defenseCost = 0;
         }
+    },
+
+    clearRemoteHarvestStats: function (source, target) {
+        if (!Memory.rooms[source]) return;
+
+        var memory = Memory.rooms[source];
+        stats.initRemoteHarvestMemory(source, target);
+
+        memory.remoteHarvesting[target].creepCost = 0;
+        memory.remoteHarvesting[target].defenseCost = 0;
+        memory.remoteHarvesting[target].buildCost = 0;
+        memory.remoteHarvesting[target].revenue = 0;
     },
 
     addRemoteHarvestCost: function (source, target, cost) {
