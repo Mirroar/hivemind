d414ac552a998f2113a12a96c9d38972a786c8c5
David Franke <mirroar@gmail.com>
2016-12-09 07:00:51 +0100

    calculate list of rooms for scouting in strategy manager

--- manager.strategy.js
+++ manager.strategy.js
@@ -1,30 +1,71 @@
 var strategyManager = {
 
-    runLogic: function () {
+  runLogic: function () {
 
-        var roomList = strategyManager.generateScoutTargets();
+    if (!Memory.strategy) {
+      Memory.strategy = {};
+    }
+    let memory = Memory.strategy;
 
-        // @todo Create scouts when no observer in range of a listed room, and send scouts to those rooms.
+    var roomList = strategyManager.generateScoutTargets();
+    memory.roomList = roomList;
 
-    },
+    // @todo Create scouts when no observer in range of a listed room, and send scouts to those rooms.
 
-    generateScoutTargets: function () {
-        let roomList = {};
+  },
 
-        // Starting point for scouting operations are owned rooms.
-        for (let roomName in Game.rooms) {
-            let room = Game.rooms[roomName];
-            if (!room.controller || !room.controller.my) continue;
+  generateScoutTargets: function () {
+    let roomList = {};
 
-            let openList = {};
-            openList[roomName] = {range: 0};
-            let closedList = {};
+    // Starting point for scouting operations are owned rooms.
+    for (let roomName in Game.rooms) {
+      let room = Game.rooms[roomName];
+      if (!room.controller || !room.controller.my || !room.memory.intel) continue;
 
-            // @todo Flood fill from center of room and add rooms we need intel of.
+      let openList = {};
+      openList[roomName] = {range: 0};
+      let closedList = {};
+
+      // @todo Flood fill from center of room and add rooms we need intel of.
+      while (_.size(openList) > 0) {
+        let minDist = null;
+        let nextRoom = null;
+        for (let rName in openList) {
+          let info = openList[rName];
+          if (!minDist || info.range < minDist) {
+            minDist = info.range;
+            nextRoom = rName;
+          }
         }
 
-        return roomList;
-    },
+        if (!nextRoom) {
+          break;
+        }
+        delete openList[nextRoom];
+        closedList[nextRoom] = true;
+
+        // Add unhandled adjacent rooms to open list.
+        if (Memory.rooms[nextRoom] && Memory.rooms[nextRoom].intel && Memory.rooms[nextRoom].intel.exits) {
+          for (let i in Memory.rooms[nextRoom].intel.exits) {
+            let exit = Memory.rooms[nextRoom].intel.exits[i];
+            if (openList[exit] || closedList[exit]) continue;
+
+            openList[exit] = {range: minDist + 1};
+          }
+        }
+
+        // Add current room as a candidate for scouting.
+        if (!roomList[nextRoom] || roomList[nextRoom].range > minDist) {
+          roomList[nextRoom] = {
+            range: minDist,
+            origin: roomName,
+          };
+        }
+      }
+    }
+
+    return roomList;
+  },
 
 };
 
