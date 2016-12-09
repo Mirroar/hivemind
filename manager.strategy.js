18ef1da9c16579546c3f0f38def06148b162bdf0
David Franke <david.franke@werk21.de>
2016-12-09 17:43:54 +0100

    speed up building of room list in strategy manager

--- manager.strategy.js
+++ manager.strategy.js
@@ -17,51 +17,66 @@ var strategyManager = {
   generateScoutTargets: function () {
     let roomList = {};
 
+    let openList = {};
+    let closedList = {};
+
     // Starting point for scouting operations are owned rooms.
     for (let roomName in Game.rooms) {
       let room = Game.rooms[roomName];
       if (!room.controller || !room.controller.my || !room.memory.intel) continue;
 
-      let openList = {};
-      openList[roomName] = {range: 0};
-      let closedList = {};
+      openList[roomName] = {
+        range: 0,
+        origin: roomName,
+      };
+    }
 
-      // @todo Flood fill from center of room and add rooms we need intel of.
-      while (_.size(openList) > 0) {
-        let minDist = null;
-        let nextRoom = null;
-        for (let rName in openList) {
-          let info = openList[rName];
-          if (!minDist || info.range < minDist) {
-            minDist = info.range;
-            nextRoom = rName;
-          }
+    // @todo Flood fill from center of room and add rooms we need intel of.
+    while (_.size(openList) > 0) {
+      let minDist = null;
+      let nextRoom = null;
+      for (let rName in openList) {
+        let info = openList[rName];
+        if (!minDist || info.range < minDist) {
+          minDist = info.range;
+          nextRoom = rName;
         }
+      }
 
-        if (!nextRoom) {
-          break;
-        }
-        delete openList[nextRoom];
-        closedList[nextRoom] = true;
+      if (!nextRoom) {
+        break;
+      }
 
-        // Add unhandled adjacent rooms to open list.
-        if (Memory.rooms[nextRoom] && Memory.rooms[nextRoom].intel && Memory.rooms[nextRoom].intel.exits) {
-          for (let i in Memory.rooms[nextRoom].intel.exits) {
-            let exit = Memory.rooms[nextRoom].intel.exits[i];
-            if (openList[exit] || closedList[exit]) continue;
+      let info = openList[nextRoom];
 
-            openList[exit] = {range: minDist + 1};
-          }
-        }
+      // Add unhandled adjacent rooms to open list.
+      if (Memory.rooms[nextRoom] && Memory.rooms[nextRoom].intel && Memory.rooms[nextRoom].intel.exits) {
+        for (let i in Memory.rooms[nextRoom].intel.exits) {
+          let exit = Memory.rooms[nextRoom].intel.exits[i];
+          if (openList[exit] || closedList[exit]) continue;
 
-        // Add current room as a candidate for scouting.
-        if (!roomList[nextRoom] || roomList[nextRoom].range > minDist) {
-          roomList[nextRoom] = {
-            range: minDist,
-            origin: roomName,
+          openList[exit] = {
+            range: info.range + 1,
+            origin: info.origin,
           };
         }
       }
+
+      delete openList[nextRoom];
+      closedList[nextRoom] = true;
+
+      // Add current room as a candidate for scouting.
+      if (!roomList[nextRoom] || roomList[nextRoom].range > info.range) {
+        roomList[nextRoom] = {
+          range: info.range,
+          origin: info.origin,
+        };
+      }
+    }
+
+    // Add data to scout list for creating priorities.
+    for (let roomName in roomList) {
+      let info = roomList[roomName];
     }
 
     return roomList;
