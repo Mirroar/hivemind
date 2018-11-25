c785232b1547eaa84a48530a177e3abcf8970c38
David Franke <mirroar@gmail.com>
2018-11-25 08:08:27 +0100

    Move room resource status functions out of structure manager file

--- src/room.prototype.resources.js
+++ src/room.prototype.resources.js
@@ -21,3 +21,75 @@ Room.prototype.getStoredEnergy = function () {
 
   return total;
 };
+
+Room.prototype.getStorageLimit = function () {
+  let total = 0;
+  if (this.storage) {
+    total = total + this.storage.storeCapacity;
+  }
+  else {
+    // Assume 10000 storage for dropping stuff on the ground.
+    total = total + 10000;
+  }
+  if (this.terminal) {
+    total = total + this.terminal.storeCapacity;
+  }
+
+  return total;
+}
+
+Room.prototype.getStorageCapacity = function () {
+  // Determines amount of free space in storage.
+  let limit = this.getStorageLimit();
+  if (this.storage) {
+    limit = limit - _.sum(this.storage.store);
+  }
+  if (this.terminal) {
+    limit = limit - _.sum(this.terminal.store);
+  }
+
+  return limit;
+}
+
+Room.prototype.getCurrentResourceAmount = function (resourceType) {
+  let total = 0;
+  if (this.storage && this.storage.store[resourceType]) {
+    total = total + this.storage.store[resourceType];
+  }
+  if (this.terminal && this.terminal.store[resourceType]) {
+    total = total + this.terminal.store[resourceType];
+  }
+
+  return total;
+}
+
+Room.prototype.getCurrentMineralAmount = function () {
+  // @todo This could use caching.
+  let total = 0;
+
+  for (let i in RESOURCES_ALL) {
+    let resourceType = RESOURCES_ALL[i];
+    if (resourceType == RESOURCE_ENERGY || resourceType == RESOURCE_POWER) continue;
+    total = total + this.getCurrentResourceAmount(resourceType);
+  }
+
+  return total;
+};
+
+Room.prototype.isFullOnEnergy = function () {
+  return this.getCurrentResourceAmount(RESOURCE_ENERGY) > this.getStorageLimit() / 2;
+};
+
+Room.prototype.isFullOnPower = function () {
+  return this.getCurrentResourceAmount(RESOURCE_ENERGY) > this.getStorageLimit() / 6;
+};
+
+Room.prototype.isFullOnMinerals = function () {
+  return this.getCurrentMineralAmount() > this.getStorageLimit() / 3;
+};
+
+Room.prototype.isFullOn = function (resourceType) {
+  if (resourceType == RESOURCE_ENERGY) return this.isFullOnEnergy();
+  if (resourceType == RESOURCE_POWER) return this.isFullOnPower();
+  return this.isFullOnMinerals();
+};
