2341bbb2944feda1f77d357566c826c113a8d127
David Franke <mirroar@gmail.com>
2019-01-24 21:10:56 +0100

    Added a couple of useful snippets

--- src/snippets.js
+++ src/snippets.js
@@ -5,3 +5,10 @@
 
 // Remove all constructions sites in a roon you have vision in:
 _.forEach(Game.rooms.E49S48.find(FIND_CONSTRUCTION_SITES), (s) => s.remove());
+
+// Find out which rooms are of interest for expansion.
+JSON.stringify(_.max(Memory.strategy.roomList, 'expansionScore'));
+JSON.stringify(_.sortBy(Memory.strategy.roomList, 'expansionScore'));
+
+// Re-run room planner for a room.
+Memory.rooms.E49S51.roomPlanner.plannerVersion = 0;
