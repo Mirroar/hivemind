fa21ac4d81e7922c588d478cca051063d1c201c2
David Franke <mirroar@gmail.com>
2019-03-18 21:54:39 +0100

    Added a couple of useful snippets.

--- src/snippets.js
+++ src/snippets.js
@@ -15,3 +15,10 @@ Memory.rooms.E49S51.roomPlanner.plannerVersion = 0;
 
 // Find out which processes use a lot of CPU
 JSON.stringify(_.sortBy(_.map(Memory.hivemind.process, (a, b) => {a.name = b; return a}), a => -a.cpu));
+
+// Find out where a lot of memory is used:
+JSON.stringify(_.sortBy(_.map(Memory, (data, key) => {return {key, size: JSON.stringify(data).length}}), 'size'));
+JSON.stringify(_.reduce(_.map(Memory.rooms, (roomData) => {const result = {}; _.each(roomData, (data, key) => result[key] = JSON.stringify(data).length); return result}), (total, item) => {_.each(item, (value, key) => total[key] = (total[key] || 0) + value); return total}));
+
+// Calculate room value.
+const p = new (require('process.strategy.scout')); p.generateMineralStatus(); Memory.hivemind.canExpand = true; const r = []; _.each(Game.rooms, room => {if (!room.controller || !room.controller.my) return; const i = p.calculateExpansionScore(room.name);i.roomName = room.name; r.push(i)}); Memory.hivemind.canExpand = false; console.log(JSON.stringify(r));
