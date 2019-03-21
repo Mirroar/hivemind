9db4ed629b545caab80fead29c111fb5c42234ad
David Franke <mirroar@gmail.com>
2019-03-21 11:35:35 +0100

    Run parts of OwnedRoomsProcess as sub processes for better checking where cpu usage is high.

--- src/process.rooms.owned.js
+++ src/process.rooms.owned.js
@@ -30,40 +30,54 @@ OwnedRoomProcess.prototype = Object.create(Process.prototype);
  * Manages on of our rooms.
  */
 OwnedRoomProcess.prototype.run = function () {
-	this.room.roomPlanner.runLogic();
+	hivemind.runSubProcess('rooms_roomplanner', () => {
+		this.room.roomPlanner.runLogic();
+	});
 
 	// @todo Only run processes based on current room level or existing structures.
-	hivemind.runProcess(this.room.name + '_defense', RoomDefenseProcess, {
-		room: this.room,
-		priority: PROCESS_PRIORITY_ALWAYS,
+	hivemind.runSubProcess('rooms_defense', () => {
+		hivemind.runProcess(this.room.name + '_defense', RoomDefenseProcess, {
+			room: this.room,
+			priority: PROCESS_PRIORITY_ALWAYS,
+		});
 	});
 
-	hivemind.runProcess(this.room.name + '_links', ManageLinksProcess, {
-		interval: 10,
-		room: this.room,
+	hivemind.runSubProcess('rooms_links', () => {
+		hivemind.runProcess(this.room.name + '_links', ManageLinksProcess, {
+			interval: 10,
+			room: this.room,
+		});
 	});
 
-	hivemind.runProcess(this.room.name + '_labs', ManageLabsProcess, {
-		room: this.room,
+	hivemind.runSubProcess('rooms_labs', () => {
+		hivemind.runProcess(this.room.name + '_labs', ManageLabsProcess, {
+			room: this.room,
+		});
 	});
 
-	// Process power in power spawns.
-	const powerSpawn = this.room.powerSpawn;
-	if (powerSpawn && powerSpawn.my && powerSpawn.power > 0 && powerSpawn.energy >= POWER_SPAWN_ENERGY_RATIO) {
-		powerSpawn.processPower();
-	}
+	hivemind.runSubProcess('rooms_power', () => {
+		// Process power in power spawns.
+		const powerSpawn = this.room.powerSpawn;
+		if (powerSpawn && powerSpawn.my && powerSpawn.power > 0 && powerSpawn.energy >= POWER_SPAWN_ENERGY_RATIO) {
+			powerSpawn.processPower();
+		}
+	});
 
-	// Use observers if requested.
-	if (this.room.observer && this.room.memory.observeTargets && this.room.memory.observeTargets.length > 0) {
-		const target = this.room.memory.observeTargets.pop();
-		this.room.observer.observeRoom(target);
-		this.room.observer.hasScouted = true;
-	}
+	hivemind.runSubProcess('rooms_observers', () => {
+		// Use observers if requested.
+		if (this.room.observer && this.room.memory.observeTargets && this.room.memory.observeTargets.length > 0) {
+			const target = this.room.memory.observeTargets.pop();
+			this.room.observer.observeRoom(target);
+			this.room.observer.hasScouted = true;
+		}
+	});
 
-	// Sing a song.
-	hivemind.runProcess(this.room.name + '_song', RoomSongsProcess, {
-		room: this.room,
-		priority: PROCESS_PRIORITY_LOW,
+	hivemind.runSubProcess('rooms_songs', () => {
+		// Sing a song.
+		hivemind.runProcess(this.room.name + '_song', RoomSongsProcess, {
+			room: this.room,
+			priority: PROCESS_PRIORITY_LOW,
+		});
 	});
 };
 
