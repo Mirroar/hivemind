0508f421f0fe6883cc6c16e4c8a8a605c2a75a06
David Franke <mirroar@gmail.com>
2019-04-01 21:39:44 +0200

    Added `handleMapArea` utility function and tests.

--- src/utilities.js
+++ src/utilities.js
@@ -722,6 +722,33 @@ const utilities = {
 		}
 	},
 
+	/**
+	 * Runs a function for every tile in range around a given center coordinate.
+	 *
+	 * @param {number} x
+	 *   Center tile's x coordinate.
+	 * @param {number} y
+	 *   Center tile's y coordinate.
+	 * @param {function} callback
+	 *   Callback that gets invoked for every tile with x and y coordinates as
+	 *   arguments. It may explicitly return false to stop looping through tiles.
+	 * @param {number} range
+	 *   (Optional) Range around the center to run code for. Defaults to 1.
+	 */
+	handleMapArea(x, y, callback, range) {
+		if (typeof range === 'undefined') range = 1;
+		for (let dx = -range; dx <= range; dx++) {
+			if (x + dx < 0) continue;
+			if (x + dx >= 50) continue;
+			for (let dy = -range; dy <= range; dy++) {
+				// Clamp to map boundaries.
+				if (y + dy < 0) continue;
+				if (y + dy >= 50) continue;
+				if (callback(x + dx, y + dy) === false) return;
+			}
+		}
+	},
+
 };
 
 module.exports = utilities;
--- test/utilities.js
+++ test/utilities.js
@@ -93,3 +93,66 @@ test('getThrottleOffset', t => {
 	t.is(typeof a, 'number', 'Throttle offsets should be numbers');
 	t.not(a, b, 'Subsequent calls should yield different offsets.');
 });
+
+test('handleMapArea', t => {
+	const tiles = {};
+	utilities.handleMapArea(5, 5, (x, y) => {
+		tiles[x + '-' + y] = true;
+	});
+	t.deepEqual(tiles, {
+		'4-4': true,
+		'4-5': true,
+		'4-6': true,
+		'5-4': true,
+		'5-5': true,
+		'5-6': true,
+		'6-4': true,
+		'6-5': true,
+		'6-6': true,
+	});
+
+	utilities.handleMapArea(6, 6, (x, y) => {
+		tiles[x + '-' + y] = false;
+	}, 0);
+	t.deepEqual(tiles, {
+		'4-4': true,
+		'4-5': true,
+		'4-6': true,
+		'5-4': true,
+		'5-5': true,
+		'5-6': true,
+		'6-4': true,
+		'6-5': true,
+		'6-6': false,
+	}, 'Allow running for a certain range.');
+
+	let counter = 0;
+	utilities.handleMapArea(10, 10, () => {
+		counter++;
+		return false;
+	});
+	t.is(counter, 1, 'Callback may return false to prevent further calls.');
+
+	counter = 0;
+	utilities.handleMapArea(0, 1, () => {
+		counter++;
+	}, 2);
+	t.is(counter, 12, 'Tiles outside of map area do not get processed.');
+
+	counter = 0;
+	utilities.handleMapArea(50, 50, () => {
+		counter++;
+	}, 2);
+	t.is(counter, 4, 'Tiles outside of map area do not get processed.');
+
+	const wrapper = {
+		test: 'foo',
+		run() {
+			utilities.handleMapArea(10, 10, () => {
+				this.test += '+';
+			});
+		},
+	};
+	wrapper.run();
+	t.is(wrapper.test, 'foo+++++++++', 'Callback preserves `this` argument.');
+});
