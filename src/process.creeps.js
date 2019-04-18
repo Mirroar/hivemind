45674397cfb51fcf79fd7f32c3df85239f018414
David Franke <mirroar@gmail.com>
2019-04-18 21:25:06 +0200

    Dynamically load creep roles.

--- src/process.creeps.js
+++ src/process.creeps.js
@@ -6,21 +6,23 @@ const Process = require('./process');
 const CreepManager = require('./creep-manager');
 
 // Normal creep roles.
-const BrawlerRole = require('./role.brawler');
-const BuilderRole = require('./role.builder');
-const ClaimerRole = require('./role.claimer');
-const DismantlerRole = require('./role.dismantler');
-const ExploitBuilderRole = require('./role.builder.exploit');
-const ExploitHarvesterRole = require('./role.harvester.exploit');
-const ExploitHaulerRole = require('./role.hauler.exploit');
-const GiftRole = require('./role.gift');
-const HarvesterRole = require('./role.harvester');
-const PowerHarvesterRole = require('./role.harvester.power');
-const PowerHaulerRole = require('./role.hauler.power');
-const RemoteBuilderRole = require('./role.builder.remote');
-const RemoteHarvesterRole = require('./role.harvester.remote');
-const ScoutRole = require('./role.scout');
-const UpgraderRole = require('./role.upgrader');
+const creepRoles = [
+	'brawler',
+	'builder',
+	'claimer',
+	'dismantler',
+	'builder.exploit',
+	'harvester.exploit',
+	'hauler.exploit',
+	'gift',
+	'harvester',
+	'harvester.power',
+	'hauler.power',
+	'builder.remote',
+	'harvester.remote',
+	'scout',
+	'upgrader',
+];
 
 // Power creep roles.
 const OperatorRole = require('./role.operator');
@@ -38,23 +40,10 @@ const CreepsProcess = function (params, data) {
 	Process.call(this, params, data);
 
 	this.creepManager = new CreepManager();
-	// @todo Require and initialize roles dynamically from a list.
-	this.creepManager.registerCreepRole('brawler', new BrawlerRole());
-	this.creepManager.registerCreepRole('builder', new BuilderRole());
-	this.creepManager.registerCreepRole('claimer', new ClaimerRole());
-	this.creepManager.registerCreepRole('dismantler', new DismantlerRole());
-	this.creepManager.registerCreepRole('builder.exploit', new ExploitBuilderRole());
-	this.creepManager.registerCreepRole('harvester.exploit', new ExploitHarvesterRole());
-	this.creepManager.registerCreepRole('hauler.exploit', new ExploitHaulerRole());
-	this.creepManager.registerCreepRole('gift', new GiftRole());
-	this.creepManager.registerCreepRole('harvester', new HarvesterRole());
-	this.creepManager.registerCreepRole('harvester.minerals', new HarvesterRole());
-	this.creepManager.registerCreepRole('harvester.power', new PowerHarvesterRole());
-	this.creepManager.registerCreepRole('hauler.power', new PowerHaulerRole());
-	this.creepManager.registerCreepRole('builder.remote', new RemoteBuilderRole());
-	this.creepManager.registerCreepRole('harvester.remote', new RemoteHarvesterRole());
-	this.creepManager.registerCreepRole('scout', new ScoutRole());
-	this.creepManager.registerCreepRole('upgrader', new UpgraderRole());
+	for (const roleName of creepRoles) {
+		const RoleClass = require('./role.' + roleName);
+		this.creepManager.registerCreepRole(roleName, new RoleClass());
+	}
 
 	this.powerCreepManager = new CreepManager();
 	this.powerCreepManager.registerCreepRole('operator', new OperatorRole());
