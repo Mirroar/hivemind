20a07bde192c2be02a3e659c9530dc561d8c645c
David Franke <mirroar@gmail.com>
2019-04-23 08:47:55 +0200

    Move containSingleRoomCreeps and boost manager logic to default preRun code so we can finally completely remove creep handling from main.old.js.

--- src/main.old.js
+++ src/main.old.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* global hivemind Creep RoomPosition TOP RIGHT BOTTOM LEFT */
+/* global Creep */
 
 /* eslint-disable import/no-unassigned-import */
 require('./manager.military');
@@ -8,145 +8,13 @@ require('./manager.source');
 /* eslint-enable import/no-unassigned-import */
 
 const spawnManager = require('./manager.spawn');
-const utilities = require('./utilities');
 
 // @todo Add a healer to defender squads, or spawn one when creeps are injured.
 
 // @todo Do not send any remote harvesters or claimers until enemies in a room should have expired. Maybe scout from time to time.
 // @todo make unarmed creeps run from hostiles.
 
-// @todo Cache building info and CostMatrix objects when scanning rooms in intel manager.
-
 // @todo Spawn creeps using "sequences" where more control is needed.
-
-// Information about how throttling works for each creep role.
-const creepThrottleLevels = {};
-const creepLogicFunctions = {};
-
-/**
- * Runs a creeps logic depending on role and other factors.
- */
-Creep.prototype.runLogic = function () {
-	const creep = this;
-
-	if (!Game.creepPerformance[this.memory.role]) {
-		Game.creepPerformance[this.memory.role] = {
-			throttled: 0,
-			count: 0,
-			cpu: 0,
-		};
-	}
-
-	if (!this.memory.throttleOffset) this.memory.throttleOffset = utilities.getThrottleOffset();
-	let minBucket = null;
-	let maxBucket = null;
-	if (creepThrottleLevels[this.memory.role]) {
-		const min = creepThrottleLevels[this.memory.role].min;
-		const max = creepThrottleLevels[this.memory.role].max;
-
-		if (min && Memory.throttleInfo.bucket[min]) {
-			minBucket = Memory.throttleInfo.bucket[min];
-		}
-		else {
-			minBucket = min;
-		}
-
-		if (max && Memory.throttleInfo.bucket[max]) {
-			maxBucket = Memory.throttleInfo.bucket[max];
-		}
-		else {
-			maxBucket = max;
-		}
-	}
-
-	if (utilities.throttle(this.memory.throttleOffset, minBucket, maxBucket)) {
-		if (creep.pos.x === 0 || creep.pos.x === 49 || creep.pos.y === 0 || creep.pos.y === 49) {
-			// Do not throttle creeps at room borders, so they don't get stuck between rooms.
-		}
-		else {
-			Game.numThrottledCreeps++;
-			Game.creepPerformance[this.memory.role].throttled++;
-			return;
-		}
-	}
-
-	if (this.containSingleRoomCreep()) return;
-
-	Game.creepPerformance[this.memory.role].count++;
-	const startTime = Game.cpu.getUsed();
-
-	this.runLogicByRole();
-
-	if (!Game.creepPerformance[this.memory.role]) {
-		Game.creepPerformance[this.memory.role] = {
-			throttled: 0,
-			count: 0,
-			cpu: 0,
-		};
-	}
-
-	Game.creepPerformance[this.memory.role].cpu += Game.cpu.getUsed() - startTime;
-};
-
-/**
- * Ensures that creeps which are restricted to a single room stay there.
- *
- * @return {boolean}
- *   True if creep is busy getting back to its room.
- */
-Creep.prototype.containSingleRoomCreep = function () {
-	if (this.memory.singleRoom && this.pos.roomName !== this.memory.singleRoom) {
-		this.moveTo(new RoomPosition(25, 25, this.memory.singleRoom));
-		return true;
-	}
-
-	if (this.memory.singleRoom && this.pos.roomName === this.memory.singleRoom) {
-		let stuck = true;
-		if (this.pos.x === 0) {
-			this.move(RIGHT);
-		}
-		else if (this.pos.y === 0) {
-			this.move(BOTTOM);
-		}
-		else if (this.pos.x === 49) {
-			this.move(LEFT);
-		}
-		else if (this.pos.y === 49) {
-			this.move(TOP);
-		}
-		else {
-			stuck = false;
-		}
-
-		if (stuck) {
-			this.say('unstuck!');
-			delete this.memory.go;
-			this.clearCachedPath();
-			return true;
-		}
-	}
-};
-
-/**
- * Runs a creep's logic code depending on role.
- */
-Creep.prototype.runLogicByRole = function () {
-	const creep = this;
-
-	try {
-		if (creep.room.boostManager && creep.room.boostManager.overrideCreepLogic(creep)) {
-			return;
-		}
-
-		if (creepLogicFunctions[creep.memory.role]) {
-			creep[creepLogicFunctions[creep.memory.role]]();
-		}
-	}
-	catch (error) {
-		console.log('Error when managing creep', creep.name, ':', error);
-		console.log(error.stack);
-	}
-};
 
 /**
  * Add additional data for each creep.
@@ -199,34 +67,11 @@ Creep.prototype.enhanceData = function () {
 const main = {
 
 	/**
-	 * Manages logic for all creeps.
-	 */
-	manageCreeps() {
-		Game.numThrottledCreeps = 0;
-		Game.creepPerformance = {};
-		_.each(Game.creeps, creep => {
-			if (!creep.spawning) creep.runLogic();
-		});
-
-		if (Game.numThrottledCreeps > 0) {
-			hivemind.log('creeps').debug(Game.numThrottledCreeps, 'of', _.size(Game.creeps), 'creeps have been throttled due to bucket this tick.');
-		}
-
-		for (const role in Game.creepPerformance) {
-			if (Game.creepPerformance[role].count > 0) {
-				Game.creepPerformance[role].avg = Game.creepPerformance[role].cpu / Game.creepPerformance[role].count;
-			}
-		}
-	},
-
-	/**
 	 * Main game loop.
 	 */
 	loop() {
 		const mainLoop = function () {
 			spawnManager.manageSpawns();
-
-			main.manageCreeps();
 		};
 
 		mainLoop();
--- src/role.js
+++ src/role.js
@@ -1,13 +1,65 @@
 'use strict';
 
+/* global RoomPosition RIGHT LEFT TOP BOTTOM */
+
 /**
  * Base class for creep roles.
  * @constructor
  */
 const Role = function () {
+	this.throttleAt = 8000;
+	this.stopAt = 2000;
 };
 
-Role.prototype.throttleAt = 8000;
-Role.prototype.stopAt = 2000;
+Role.prototype.preRun = function (creep) {
+	if (this.containSingleRoomCreep(creep)) return false;
+
+	if (creep.room.boostManager && creep.room.boostManager.overrideCreepLogic(creep)) {
+		return false;
+	}
+};
+
+/**
+ * Ensures that creeps which are restricted to a single room stay there.
+ *
+ * @param {Creep} creep
+ *   The creep to run logic for.
+ *
+ * @return {boolean}
+ *   True if creep is busy getting back to its room.
+ */
+Role.prototype.containSingleRoomCreep = function (creep) {
+	if (!creep.memory.singleRoom) return false;
+
+	if (creep.pos.roomName === creep.memory.singleRoom) {
+		let stuck = true;
+		if (creep.pos.x === 0) {
+			creep.move(RIGHT);
+		}
+		else if (creep.pos.y === 0) {
+			creep.move(BOTTOM);
+		}
+		else if (creep.pos.x === 49) {
+			creep.move(LEFT);
+		}
+		else if (creep.pos.y === 49) {
+			creep.move(TOP);
+		}
+		else {
+			stuck = false;
+		}
+
+		if (stuck) {
+			creep.say('unstuck!');
+			delete creep.memory.go;
+			creep.clearCachedPath();
+			return true;
+		}
+	}
+	else {
+		creep.moveTo(new RoomPosition(25, 25, creep.memory.singleRoom));
+		return true;
+	}
+};
 
 module.exports = Role;
