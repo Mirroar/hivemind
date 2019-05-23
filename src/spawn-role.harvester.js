357997918f41faf97c830640012c3c596acfb665
David Franke <mirroar@gmail.com>
2019-05-23 09:19:01 +0200

    Documentation and slight refactoring of harvester spawn role.

--- src/spawn-role.harvester.js
+++ src/spawn-role.harvester.js
@@ -1,17 +1,23 @@
 'use strict';
 
-const HarvesterSpawnRole = function () {
+/**
+ * @constructor
+ */
+const HarvesterSpawnRole = function () {};
 
-};
-
+/**
+ * Adds spawn options for the given room.
+ *
+ * @param {Room} room
+ *   The room to add spawn options for.
+ * @param {Object[]} options
+ *   A list of spawn options to add to.
+ */
 HarvesterSpawnRole.prototype.getSpawnOptions = function (room, options) {
-	// If we have no other way to recover, spawn with reduced amounts of parts.
-	let force = false;
-	if (_.size(room.creepsByRole.harvester) === 0 && (!room.storage || (_.size(room.creepsByRole.transporter) === 0 && room.getStoredEnergy() < 5000))) {
-		force = true;
-	}
+	const force = this.isSmallHarvesterNeeded(room);
 
-	if (!force && room.isFullOnEnergy()) return;
+	// Stop harvesting if we can't really store any more energy.
+	if (room.isFullOnEnergy() && !force) return;
 
 	for (const source of room.sources) {
 		const maxParts = source.getMaxWorkParts();
@@ -54,6 +60,31 @@ HarvesterSpawnRole.prototype.getSpawnOptions = function (room, options) {
 			});
 		}
 	}
+};
+
+/**
+ * Decides whether we have no other way to recover but to spawn with a reduced
+ * number of parts.
+ *
+ * @param {Room} room
+ *   The room to check.
+ *
+ * @return {boolean}
+ *   True if a small harvester should be spawned.
+ */
+HarvesterSpawnRole.prototype.isSmallHarvesterNeeded = function (room) {
+	// If there's another harvester, we're fine.
+	if (_.size(room.creepsByRole.harvester) > 0) return false;
+
+	// Otherwise, rooms without a storage need a harvester always.
+	if (!room.storage) return true;
+
+	// Rooms with a storage need to have some energy left. In that case,
+	// a transporter can be spawned and provide enough energy for a full
+	// harvester.
+	if (room.getStoredEnergy() < 5000) return true;
+
+	return false;
 };
 
 module.exports = HarvesterSpawnRole;
