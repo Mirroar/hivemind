2eb60cf134a574463b117c499a16fe63a74e04f1
David Franke <mirroar@gmail.com>
2021-02-26 23:02:47 +0100

    Code style fixes.

--- src/process.strategy.mining.js
+++ src/process.strategy.mining.js
@@ -44,7 +44,7 @@ RemoteMiningProcess.prototype.run = function () {
 
 		// In our first room, don't start remote harvesting immediately. The energy
 		// required to start being profitable is better used for upgrading.
-		if (_.size(Game.spawns) === 1 && _.sample(Game.spawns).room.name == room.name && room.controller.level < 4) return;
+		if (_.size(Game.spawns) === 1 && _.sample(Game.spawns).room.name === room.name && room.controller.level < 4) return;
 
 		const numSpawns = _.filter(Game.spawns, spawn => spawn.pos.roomName === room.name && spawn.isOperational()).length;
 		if (numSpawns === 0) return;
@@ -85,6 +85,7 @@ RemoteMiningProcess.prototype.run = function () {
 			// Harvest from this room.
 			info.harvestActive = true;
 		}
+
 		availableHarvestRoomCount++;
 	}
 
