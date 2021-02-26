27d59c5e75a956dd55d7e0d95048c5525e6221ba
David Franke <mirroar@gmail.com>
2021-02-26 23:07:31 +0100

    Add gatherer role to gather resources left in other rooms.

---
+++ src/role.gatherer.js
@@ -0,0 +1,164 @@
+'use strict';
+
+/* global FIND_STRUCTURES FIND_RUINS FIND_DROPPED_RESOURCES */
+
+const Role = require('./role');
+
+/**
+ * Gatherers collect resources from safe sources outside their spawn room.
+ *
+ * They do no work to "produce" these resources, instead relying on gathered
+ * intel about resources left in buildings or ruins.
+ * A gatherer will move directly to the target room, choose a target to withdraw
+ * from, and return home once full to deposit the gathered resources.
+ *
+ * Memory structure:
+ * - origin: Name of the room the creep originates in.
+ * - targetRoom: Name of the room to gather resources in.
+ */
+module.exports = class GathererRole extends Role {
+	/**
+	 * Makes this creep behave like a gatherer.
+	 *
+	 * @param {Creep} creep
+	 *   The creep to run logic for.
+	 */
+	run(creep) {
+		if (creep.memory.delivering) {
+			this.deliverResources(creep);
+			return;
+		}
+
+		this.gatherResources(creep);
+	}
+
+	/**
+	 * Makes the creep move into the target room and gather resources.
+	 *
+	 * @param {Creep} creep
+	 *   The creep to run logic for.
+	 */
+	gatherResources(creep) {
+		if (creep.pos.roomName !== creep.memory.targetRoom) {
+			// Move back to spawn room.
+			creep.moveToRoom(creep.memory.targetRoom);
+			return;
+		}
+
+		// Choose a target in the room.
+		const target = this.getGatherTarget(creep);
+		this.gatherFromTarget(creep, target);
+	}
+
+	/**
+	 * Chooses a target to gather resources from.
+	 *
+	 * @param {Creep} creep
+	 *   The creep to run logic for.
+	 *
+	 * @return {RoomObject}
+	 *   An object that has gatherable resources stored.
+	 */
+	getGatherTarget(creep) {
+		if (this.memory.target) {
+			const target = Game.getObjectById(this.memory.target);
+			if (target) return target;
+		}
+
+		const resources = creep.room.find(FIND_DROPPED_RESOURCES);
+		const structures = creep.room.find(FIND_STRUCTURES);
+		const ruins = creep.room.find(FIND_RUINS);
+
+		// @todo Decide what the most valuable target is.
+		for (const resource of resources) {
+			// @todo
+			if (resource.amount) {
+				this.memory.target = resource.id;
+				return resource;
+			}
+		}
+
+		for (const structure of structures) {
+			// @todo
+			if (structure.store && structure.store.getUsedCapacity() > 0) {
+				this.memory.target = structure.id;
+				return structure;
+			}
+		}
+
+		for (const ruin of ruins) {
+			// @todo
+			if (ruin.store && ruin.store.getUsedCapacity() > 0) {
+				this.memory.target = ruin.id;
+				return ruin;
+			}
+		}
+
+		// @todo If there's no valid target, deliver and/or assign to new room.
+		this.memory.delivering = true;
+	}
+
+	/**
+	 * Gathers resources from the given target.
+	 *
+	 * @param {Creep} creep
+	 *   The creep to run logic for.
+	 * @param {RoomObject} target
+	 *   An object that has gatherable resources stored.
+	 */
+	gatherFromTarget(creep, target) {
+		if (creep.pos.getRangeTo(target) > 1) {
+			creep.moveToRange(target, 1);
+			return;
+		}
+
+		if (target.amount) {
+			creep.pickup(target);
+		}
+
+		// @todo Withdraw as many resources as possible.
+		// @todo Start with most valuable resources?
+		_.each(target.store, (amount, resourceType) => {
+			if (!amount || amount === 0) return;
+			creep.withdraw(target, resourceType);
+		});
+
+		// Switch to delivery mode if storage is full.
+		if (creep.store.getFreeCapacity() === 0) {
+			this.memory.delivering = true;
+		}
+	}
+
+	/**
+	 * Makes the creep return to the spawn room and deliver resources.
+	 *
+	 * @param {Creep} creep
+	 *   The creep to run logic for.
+	 */
+	deliverResources(creep) {
+		if (creep.pos.roomName !== creep.memory.origin) {
+			// Move back to spawn room.
+			creep.moveToRoom(creep.memory.origin);
+			return;
+		}
+
+		// Choose a resource and deliver it.
+		_.each(creep.store, (amount, resourceType) => {
+			if (!amount || amount === 0) return;
+
+			const target = creep.room.getBestStorageTarget(amount, resourceType);
+			if (!target) return;
+
+			if (creep.pos.getRangeTo(target) > 1) {
+				creep.moveToRange(target, 1);
+				return false;
+			}
+
+			creep.transfer(target, resourceType);
+		});
+
+		if (creep.store.getUsedCapacity() === 0) {
+			this.memory.delivering = false;
+		}
+	}
+};
---
+++ src/spawn-role.gatherer.js
@@ -0,0 +1,76 @@
+'use strict';
+
+/* global MOVE CARRY */
+
+const SpawnRole = require('./spawn-role');
+
+module.exports = class GathererSpawnRole extends SpawnRole {
+	/**
+	 * Adds gatherer spawn options for the given room.
+	 *
+	 * @param {Room} room
+	 *   The room to add spawn options for.
+	 * @param {Object[]} options
+	 *   A list of spawn options to add to.
+	 */
+	getSpawnOptions(room, options) {
+		_.each(room.memory.abandonedResources, (resources, roomName) => {
+			const numGatherers = _.filter(Game.creepsByRole.gatherer || [], creep => creep.memory.targetRoom === roomName && creep.memory.origin === room.name).length;
+			// @todo Allow more gatherers at low priority if a lot of resources need
+			// gathering.
+			if (numGatherers > 0) return;
+
+			options.push({
+				priority: 2,
+				weight: 0.8,
+				targetRoom: roomName,
+			});
+		});
+	}
+
+	/**
+	 * Gets the body of a creep to be spawned.
+	 *
+	 * @param {Room} room
+	 *   The room to add spawn options for.
+	 * @param {Object} option
+	 *   The spawn option for which to generate the body.
+	 *
+	 * @return {string[]}
+	 *   A list of body parts the new creep should consist of.
+	 */
+	getCreepBody(room) {
+		return this.generateCreepBodyFromWeights(
+			this.getBodyWeights(),
+			Math.max(room.energyCapacityAvailable * 0.9, room.energyAvailable),
+		);
+	}
+
+	/**
+	 * Determine body weights for haulers.
+	 *
+	 * @return {object}
+	 *   An object containing body part weights, keyed by type.
+	 */
+	getBodyWeights() {
+		return {[MOVE]: 0.5, [CARRY]: 0.5};
+	}
+
+	/**
+	 * Gets memory for a new creep.
+	 *
+	 * @param {Room} room
+	 *   The room to add spawn options for.
+	 * @param {Object} option
+	 *   The spawn option for which to generate the body.
+	 *
+	 * @return {Object}
+	 *   The boost compound to use keyed by body part type.
+	 */
+	getCreepMemory(room, option) {
+		return {
+			origin: room.name,
+			targetRoom: option.targetRoom,
+		};
+	}
+};
