e395ea9fd258c5c128ee2a63ff103fc9f729d6a0
David Franke <mirroar@gmail.com>
2021-08-11 21:22:00 +0200

    Power haulers may now pick up power outside their target room, as well as other resources with lower priority.

--- src/role.hauler.power.js
+++ src/role.hauler.power.js
@@ -1,7 +1,8 @@
 'use strict';
 
-/* global hivemind RoomPosition RESOURCE_POWER FIND_STRUCTURES OK
-STRUCTURE_POWER_BANK FIND_DROPPED_RESOURCES FIND_RUINS */
+/* global hivemind RoomPosition RESOURCE_POWER FIND_STRUCTURES OK ATTACK_POWER
+STRUCTURE_POWER_BANK FIND_DROPPED_RESOURCES FIND_RUINS MAX_CREEP_SIZE
+FIND_TOMBSTONES */
 
 const Role = require('./role');
 
@@ -18,6 +19,12 @@ PowerHaulerRole.prototype = Object.create(Role.prototype);
  *   The creep to run logic for.
  */
 PowerHaulerRole.prototype.run = function (creep) {
+	if (!creep.memory.isReturning && (creep.store.getFreeCapacity() === 0 || (creep.store.power || 0) > creep.store.getCapacity() / 10)) {
+		// Return home.
+		creep.memory.isReturning = true;
+		return;
+	}
+
 	if (creep.memory.isReturning) {
 		this.returnHome(creep);
 		return;
@@ -25,6 +32,11 @@ PowerHaulerRole.prototype.run = function (creep) {
 
 	const targetPosition = new RoomPosition(25, 25, creep.memory.targetRoom);
 	const isInTargetRoom = creep.pos.roomName === targetPosition.roomName;
+
+	// Pick up dropped power in rooms we pass.
+	if (!isInTargetRoom && this.pickupResources(creep, RESOURCE_POWER)) return;
+
+	// Get to target room.
 	if (!isInTargetRoom || (!creep.isInRoom() && creep.getNavMeshMoveTarget())) {
 		if (creep.moveUsingNavMesh(targetPosition) !== OK) {
 			hivemind.log('creeps').debug(creep.name, 'can\'t move from', creep.pos.roomName, 'to', targetPosition.roomName);
@@ -42,8 +54,31 @@ PowerHaulerRole.prototype.run = function (creep) {
 	});
 
 	if (powerBanks.length > 0) {
-		// Wait close by until power bank is destroyed.
 		const powerBank = powerBanks[0];
+		// Get close to power bank if it's close to being destoryed.
+		if (powerBank.hits < ATTACK_POWER * MAX_CREEP_SIZE * 5) {
+			if (creep.pos.getRangeTo(powerBank) > 1) {
+				creep.moveToRange(powerBank, 1);
+			}
+
+			// Also drop anything that's not power, it can be picked up again once
+			// power is depleted.
+			if (creep.store.getUsedCapacity() > (creep.store[RESOURCE_POWER] || 0)) {
+				for (const resourceType of _.keys(creep.store)) {
+					if (resourceType === RESOURCE_POWER) continue;
+					if (!creep.store[resourceType]) continue;
+					creep.drop(resourceType);
+					break;
+				}
+			}
+
+			return;
+		}
+
+		// Pick up things in the room while waiting.
+		if (this.pickupResources(creep)) return;
+
+		// Wait close by until power bank is destroyed.
 		if (creep.pos.getRangeTo(powerBank) > 5) {
 			creep.moveToRange(powerBank, 5);
 		}
@@ -63,6 +98,10 @@ PowerHaulerRole.prototype.run = function (creep) {
 PowerHaulerRole.prototype.returnHome = function (creep) {
 	const targetPosition = new RoomPosition(25, 25, creep.memory.sourceRoom);
 	const isInTargetRoom = creep.pos.roomName === targetPosition.roomName;
+
+	// Pick up dropped power in rooms we pass.
+	if (!isInTargetRoom && creep.store.getFreeCapacity() > 0 && this.pickupResources(creep, RESOURCE_POWER)) return;
+
 	if (!isInTargetRoom || (!creep.isInRoom() && creep.getNavMeshMoveTarget())) {
 		if (creep.moveUsingNavMesh(targetPosition) !== OK) {
 			hivemind.log('creeps').debug(creep.name, 'can\'t move from', creep.pos.roomName, 'to', targetPosition.roomName);
@@ -107,32 +146,6 @@ PowerHaulerRole.prototype.pickupPower = function (creep) {
 	const powerResources = creep.room.find(FIND_DROPPED_RESOURCES, {
 		filter: resource => resource.resourceType === RESOURCE_POWER,
 	});
-	const powerRuins = creep.room.find(FIND_RUINS, {
-		filter: ruin => (ruin.store.power || 0) > 0,
-	});
-
-	if (creep.store.getFreeCapacity() === 0 || powerResources.length + powerRuins.length === 0) {
-		// @todo Loot anything else nearby, like tombstones and dropped resources.
-		// Return home.
-		creep.memory.isReturning = true;
-		return;
-	}
-
-	if (powerResources.length + powerRuins.length === 0) {
-		// Return home.
-		if (_.sum(creep.carry) > 0) {
-			creep.memory.isReturning = true;
-		}
-
-		// Mark operation as finished.
-		if (Memory.strategy && Memory.strategy.power && Memory.strategy.power.rooms && Memory.strategy.power.rooms[creep.memory.targetRoom]) {
-			Memory.strategy.power.rooms[creep.memory.targetRoom].isActive = false;
-			Memory.strategy.power.rooms[creep.memory.targetRoom].amount = 0;
-		}
-
-		return;
-	}
-
 	if (powerResources.length > 0) {
 		if (creep.pos.getRangeTo(powerResources[0]) > 1) {
 			creep.moveToRange(powerResources[0], 1);
@@ -140,8 +153,12 @@ PowerHaulerRole.prototype.pickupPower = function (creep) {
 		}
 
 		creep.pickup(powerResources[0]);
+		return;
 	}
 
+	const powerRuins = creep.room.find(FIND_RUINS, {
+		filter: ruin => (ruin.store.power || 0) > 0,
+	});
 	if (powerRuins.length > 0) {
 		if (creep.pos.getRangeTo(powerRuins[0]) > 1) {
 			creep.moveToRange(powerRuins[0], 1);
@@ -149,7 +166,72 @@ PowerHaulerRole.prototype.pickupPower = function (creep) {
 		}
 
 		creep.withdraw(powerRuins[0], RESOURCE_POWER);
+		return;
 	}
+
+	// Mark operation as finished.
+	if (Memory.strategy && Memory.strategy.power && Memory.strategy.power.rooms && Memory.strategy.power.rooms[creep.memory.targetRoom]) {
+		Memory.strategy.power.rooms[creep.memory.targetRoom].isActive = false;
+		Memory.strategy.power.rooms[creep.memory.targetRoom].amount = 0;
+	}
+
+	// Loot anything else nearby, like tombstones and dropped resources.
+	if (this.pickupResources(creep)) return;
+
+	// Return home.
+	creep.memory.isReturning = true;
+};
+
+PowerHaulerRole.prototype.pickupResources = function (creep, resourceType) {
+	let target;
+	if (creep.memory.pickupTarget) {
+		target = Game.getObjectById(creep.memory.pickupTarget);
+
+		if (!target) {
+			delete creep.memory.pickupTarget;
+		}
+		else if (target.store && (target.store.getUsedCapacity() === 0 || (resourceType && (target.store[resourceType] || 0) === 0))) {
+			// Target doesn't contain the required resource anymore.
+			delete creep.memory.pickupTarget;
+			target = null;
+		}
+	}
+
+	if (!target) {
+		const resources = creep.room.find(FIND_DROPPED_RESOURCES, {
+			filter: resource => resource.amount >= 10 && (!resourceType || resource.resourceType === resourceType),
+		});
+		const ruins = creep.room.find(FIND_RUINS, {
+			filter: ruin => ruin.store.getUsedCapacity(resourceType) >= 10,
+		});
+		const tombs = creep.room.find(FIND_TOMBSTONES, {
+			filter: tomb => tomb.store.getUsedCapacity(resourceType) >= 10,
+		});
+
+		for (const collection of [resources, ruins, tombs]) {
+			if (collection.length === 0) continue;
+
+			creep.memory.pickupTarget = collection[0].id;
+			target = collection[0];
+			break;
+		}
+	}
+
+	if (!target) return false;
+
+	if (creep.pos.getRangeTo(target.pos) > 1) {
+		creep.moveToRange(target, 1);
+		return true;
+	}
+
+	if (target.amount) {
+		creep.pickup(target);
+	}
+	else {
+		creep.withdraw(target, resourceType || _.keys(target.store)[0]);
+	}
+
+	return true;
 };
 
 module.exports = PowerHaulerRole;
