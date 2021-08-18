413b2c672b3a6b7eb76d1e2416889b45a0424bac
David Franke <mirroar@gmail.com>
2021-08-18 23:42:08 +0200

    Don't limit emergency rampart repairs by tower range.

--- src/process.rooms.owned.defense.ts
+++ src/process.rooms.owned.defense.ts
@@ -40,7 +40,6 @@ RoomDefenseProcess.prototype.manageTowers = function () {
 	if (towers.length === 0) return;
 
 	const hostileCreeps = this.room.find(FIND_HOSTILE_CREEPS);
-	const maxRepairRange = TOWER_OPTIMAL_RANGE + ((TOWER_FALLOFF_RANGE - TOWER_OPTIMAL_RANGE) / 2);
 	for (const tower of towers) {
 		// Attack enemies.
 		if (hostileCreeps.length > 0) {
@@ -74,12 +73,12 @@ RoomDefenseProcess.prototype.manageTowers = function () {
 
 				if (creep.getActiveBodyparts(RANGED_ATTACK) > 0) {
 					availableRamparts = availableRamparts.concat(creep.pos.findInRange(FIND_MY_STRUCTURES, 3, {
-						filter: s => s.structureType === STRUCTURE_RAMPART && s.pos.getRangeTo(tower.pos) <= maxRepairRange,
+						filter: s => s.structureType === STRUCTURE_RAMPART,
 					}));
 				}
 				else {
 					availableRamparts = availableRamparts.concat(creep.pos.findInRange(FIND_MY_STRUCTURES, 1, {
-						filter: s => s.structureType === STRUCTURE_RAMPART && s.pos.getRangeTo(tower.pos) <= maxRepairRange,
+						filter: s => s.structureType === STRUCTURE_RAMPART,
 					}));
 				}
 			}
