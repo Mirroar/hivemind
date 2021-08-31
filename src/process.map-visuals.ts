30317d9e5342863c3916905818001f146d888fe0
David Franke <mirroar@gmail.com>
2021-08-31 23:32:42 +0200

    Draw expansions and failed expansions on the map.

--- src/process.map-visuals.ts
+++ src/process.map-visuals.ts
@@ -1,5 +1,6 @@
 /* global RoomPosition */
 
+import cache from './cache';
 import hivemind from './hivemind';
 import Process from './process';
 import utilities from './utilities';
@@ -20,20 +21,30 @@ export default class MapVisualsProcess extends Process {
 	run() {
 		if (!enableMapVisuals) return;
 
-		// We need to check a combination of entries in room memory, and those
-		// contained in Memory.strategy.roomList.
-		_.each(Memory.strategy.roomList, (info, roomName) => {
-			if (typeof roomName !== 'string') return;
-			this.drawIntelStatus(roomName);
-			this.drawExpansionStatus(roomName);
+		let drawn = false;
+		const visuals = cache.inHeap('map-visuals', 10, () => {
+			this.drawExpansionStatus();
+
+			// We need to check a combination of entries in room memory, and those
+			// contained in Memory.strategy.roomList.
+			_.each(Memory.strategy.roomList, (info, roomName) => {
+				if (typeof roomName !== 'string') return;
+				this.drawIntelStatus(roomName);
+				this.drawRoomStatus(roomName);
+			});
+
+			_.each(_.filter(Memory.rooms, (mem, roomName) => !Memory.strategy.roomList[roomName]), (mem, roomName) => {
+				if (typeof roomName !== 'string') return;
+				this.drawIntelStatus(roomName);
+			});
+
+			this.drawNavMesh();
+
+			drawn = true;
+			return Game.map.visual.export();
 		});
 
-		_.each(_.filter(Memory.rooms, (mem, roomName) => !Memory.strategy.roomList[roomName]), (mem, roomName) => {
-			if (typeof roomName !== 'string') return;
-			this.drawIntelStatus(roomName);
-		});
-
-		this.drawNavMesh();
+		if (!drawn) Game.map.visual.import(visuals);
 	}
 
 	/**
@@ -59,7 +70,7 @@ export default class MapVisualsProcess extends Process {
 	 * @param {string} roomName
 	 *   Name of the room in question.
 	 */
-	drawExpansionStatus(roomName) {
+	drawRoomStatus(roomName) {
 		const info = Memory.strategy.roomList[roomName];
 
 		if (drawMiningStatus && (Memory.strategy.remoteHarvesting.rooms || []).indexOf(roomName) !== -1) {
@@ -70,6 +81,29 @@ export default class MapVisualsProcess extends Process {
 		if (info.expansionScore < expansionScoreCutoff) return;
 
 		Game.map.visual.text(info.expansionScore.toPrecision(3), new RoomPosition(8, 4, roomName), {fontSize: 7, align: 'left'});
+	}
+
+	drawExpansionStatus() {
+		if (!Memory.strategy.expand) return;
+
+		// Visualize failed expansion attempts.
+		for (const attempt of Memory.strategy.expand.failedExpansions || []) {
+			Game.map.visual.circle(new RoomPosition(25, 25, attempt.roomName), {fill: '#ff0000', opacity: 0.2, radius: 25});
+			Game.map.visual.circle(new RoomPosition(25, 25, attempt.roomName), {fill: '#ff0000', opacity: 0.2, radius: 125});
+		}
+
+		// Visualize current expansion target.
+		if (Memory.strategy.expand.currentTarget) {
+			const targetMemory = Memory.strategy.expand.currentTarget;
+			const targetPosition = new RoomPosition(10, 10, targetMemory.roomName);
+
+			for (const roomName of targetMemory.supportingRooms || []) {
+				Game.map.visual.line(new RoomPosition(25, 25, roomName), targetPosition, {opacity: 0.35, lineStyle: 'dashed'});
+			}
+
+			Game.map.visual.line(new RoomPosition(25, 25, targetMemory.spawnRoom), targetPosition, {opacity: 0.5});
+			Game.map.visual.text('🏴', targetPosition);
+		}
 	}
 
 	/**
