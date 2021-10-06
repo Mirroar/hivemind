d8be51cfc5e5df2e5d32eb2f6b62425252e27688
David Franke <mirroar@gmail.com>
2021-10-06 23:08:03 +0200

    Refactor reclaim detection and ignore expansion target rooms.

--- src/process/strategy/reclaim.ts
+++ src/process/strategy/reclaim.ts
@@ -12,33 +12,62 @@ export default class ReclaimProcess extends Process {
    * Sends builders to destroyed rooms we still have control over.
    */
   run() {
-    this.detectReclaimableRooms();
+    this.markReclaimableRooms();
   }
 
   /**
    * Keeps a record of reclaimable rooms.
    */
-  detectReclaimableRooms() {
+  markReclaimableRooms() {
     for (const room of Game.myRooms) {
-      if (room.find(FIND_MY_STRUCTURES, {
-        filter: structure => structure.structureType === STRUCTURE_SPAWN
-      }).length > 0) {
-        if (room.memory.isReclaimableSince && room.roomManager && room.roomManager.checkWallIntegrity()) {
-          // Room has finished rebuilding.
-          delete room.memory.isReclaimableSince;
-        }
-
+      if (this.needsReclaiming(room)) {
+        this.updateReclaimTimer(room);
         continue;
       }
 
-      if (!room.memory.isReclaimableSince) room.memory.isReclaimableSince = Game.time;
-
-      for (const username in room.enemyCreeps) {
-        if (!hivemind.relations.isAlly(username)) {
-          room.memory.isReclaimableSince = Game.time;
-          break;
-        }
+      if (this.hasJustFinishedReclaiming(room)) {
+        delete room.memory.isReclaimableSince;
       }
     }
   }
+
+  needsReclaiming(room: Room): boolean {
+    if (this.hasSpawn(room)) return false;
+    if (this.isExpansionTarget(room)) return false;
+
+    return true;
+  }
+
+  hasSpawn(room: Room): boolean {
+    return room.find(FIND_MY_STRUCTURES, {
+      filter: structure => structure.structureType === STRUCTURE_SPAWN
+    }).length > 0;
+  }
+
+  isExpansionTarget(room: Room): boolean {
+    if (!Memory.strategy) return false;
+    if (!Memory.strategy.expand) return false;
+    if (!Memory.strategy.expand.currentTarget) return false;
+
+    return room.name === Memory.strategy.expand.currentTarget.roomName;
+  }
+
+  updateReclaimTimer(room: Room) {
+    if (!room.memory.isReclaimableSince) room.memory.isReclaimableSince = Game.time;
+
+    for (const username in room.enemyCreeps) {
+      if (!hivemind.relations.isAlly(username)) {
+        room.memory.isReclaimableSince = Game.time;
+        break;
+      }
+    }
+  }
+  hasJustFinishedReclaiming(room: Room): boolean {
+    if (!room.memory.isReclaimableSince) return false;
+    if (!room.roomManager) return false;
+    if (!room.roomManager.checkWallIntegrity()) return false;
+
+    return true;
+  }
+
 }
