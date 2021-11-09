38bf27da3e0deca2de2c34b816266cc01505f04f
David Franke <mirroar@gmail.com>
2021-11-09 22:26:38 +0100

    Convert relations to a class with types.

--- src/relations.ts
+++ src/relations.ts
@@ -1,29 +1,31 @@
 import localRelations from 'relations.local';
 
-/**
- * Relations determine how we act towards other users.
- * @constructor
- */
-const Relations = function () {
-	this.allies = [];
+export default class Relations {
+	allies: string[];
 
-	if (localRelations && localRelations.allies) {
-		for (const ally of localRelations.allies) {
-			this.allies.push(ally);
+	/**
+	 * Relations determine how we act towards other users.
+	 * @constructor
+	 */
+	constructor() {
+		this.allies = [];
+
+		if (localRelations && localRelations.allies) {
+			for (const ally of localRelations.allies) {
+				this.allies.push(ally);
+			}
 		}
-	}
-};
+	};
 
-/**
- * Checks if a user is considered our ally.
- *
- * @param {string} username
- *   The name of the user to check.
- *
- * @return {boolean} true if the user is our ally.
- */
-Relations.prototype.isAlly = function (username) {
-	return this.allies.indexOf(username) !== -1;
-};
-
-export default Relations;
+	/**
+	 * Checks if a user is considered our ally.
+	 *
+	 * @param {string} username
+	 *   The name of the user to check.
+	 *
+	 * @return {boolean} true if the user is our ally.
+	 */
+	isAlly(username: string): boolean {
+		return this.allies.indexOf(username) !== -1;
+	};
+}
