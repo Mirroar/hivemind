50cb3a11782fbb9cd1d10151e922966ee53479ef
David Franke <mirroar@gmail.com>
2022-01-24 09:27:57 +0100

    Store room plans in segmented memory.

--- src/room/planner/room-plan-management.ts
+++ src/room/planner/room-plan-management.ts
@@ -1,22 +1,11 @@
 declare global {
-  interface Room {
-    getRoomPlan(): RoomPlanWithVersion | null;
-    setRoomPlan(plan: RoomPlan, version: number);
-  }
-
-  interface RoomMemory {
-    roomPlan: {
-      plan: SerializedPlan;
-      version: number;
-    };
-  }
-
   type RoomPlanWithVersion = {
     plan: RoomPlan;
     version: number;
   }
 }
 
+import hivemind from 'hivemind';
 import RoomPlan from 'room/planner/room-plan';
 
 const roomPlanCache: {
@@ -24,39 +13,33 @@ const roomPlanCache: {
 } = {};
 
 function getRoomPlanFor(roomName: string): RoomPlanWithVersion | null {
-  if (!Memory.rooms[roomName]) return null;
-  if (!Memory.rooms[roomName].roomPlan) return null;
+  if (!hivemind.segmentMemory.isReady()) return null;
+
+  const key = 'room-plan:' + roomName;
+  if (!hivemind.segmentMemory.has(key)) return null;
 
   if (!roomPlanCache[roomName]) {
-    const plan = new RoomPlan(roomName, Memory.rooms[roomName].roomPlan.plan);
-    roomPlanCache[roomName] = {plan, version: Memory.rooms[roomName].roomPlan.version};
+    const saved: {plan: SerializedPlan, version: number} = hivemind.segmentMemory.get(key);
+    const plan = new RoomPlan(roomName, saved.plan);
+    roomPlanCache[roomName] = {plan, version: saved.version};
   }
 
   return roomPlanCache[roomName];
 }
 
 function setRoomPlanFor(roomName: string, plan: RoomPlan, version: number) {
-  if (!Memory.rooms[roomName]) Memory.rooms[roomName] = {} as RoomMemory;
-
+  const key = 'room-plan:' + roomName;
   if (!plan) {
-    delete Memory.rooms[roomName].roomPlan;
+    hivemind.segmentMemory.delete(key);
     delete roomPlanCache[roomName];
     return;
   }
 
-  Memory.rooms[roomName].roomPlan = {
+  hivemind.segmentMemory.set(key, {
     plan: plan.serialize(),
     version,
-  };
+  });
   roomPlanCache[roomName] = {plan, version};
-}
-
-Room.prototype.getRoomPlan = function (this: Room) {
-  return getRoomPlanFor(this.name);
-}
-
-Room.prototype.setRoomPlan = function(this: Room, plan: RoomPlan, version: number) {
-  setRoomPlanFor(this.name, plan, version);
 }
 
 export {
