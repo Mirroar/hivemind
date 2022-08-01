eb9aeea81e075f46eb8c8ee157815add6cb31c7c
David Franke <mirroar@gmail.com>
2022-08-01 14:06:32 +0200

    Whitespace fixes.

--- src/player-intel-manager.ts
+++ src/player-intel-manager.ts
@@ -2,24 +2,24 @@ import PlayerIntel from './player-intel';
 import hivemind from './hivemind';
 
 export default class PlayerIntelManager {
-  intelCache: Record<string, PlayerIntel> = {};
+	intelCache: Record<string, PlayerIntel> = {};
 
-  /**
-   * Factory method for player intel objects.
-   *
-   * @param {string} userName
-   *   The user for whom to get intel.
-   *
-   * @return {PlayerIntel}
-   *   The requested PlayerIntel object.
-   */
-  get(userName: string): PlayerIntel {
-  	if (!hivemind.segmentMemory.isReady()) throw new Error('Memory is not ready to generate player intel for user "' + userName + '".');
+	/**
+	 * Factory method for player intel objects.
+	 *
+	 * @param {string} userName
+	 *   The user for whom to get intel.
+	 *
+	 * @return {PlayerIntel}
+	 *   The requested PlayerIntel object.
+	 */
+	get(userName: string): PlayerIntel {
+		if (!hivemind.segmentMemory.isReady()) throw new Error('Memory is not ready to generate player intel for user "' + userName + '".');
 
-  	if (!this.intelCache[userName]) {
-  		this.intelCache[userName] = new PlayerIntel(userName);
-  	}
+		if (!this.intelCache[userName]) {
+			this.intelCache[userName] = new PlayerIntel(userName);
+		}
 
-  	return this.intelCache[userName];
-  }
+		return this.intelCache[userName];
+	}
 }
