5d3658b5ef6cd68375c475f789bb5c50d235d742
David Franke <mirroar@gmail.com>
2023-02-08 12:03:08 +0100

    Move SpawnManager into DI container.

--- src/container-factory.ts
+++ src/container-factory.ts
@@ -1,6 +1,8 @@
 import container from './utils/container';
 import PlayerIntelManager from './player-intel-manager';
+import SpawnManager from './spawn-manager';
 
 export default function () {
 	container.set('PlayerIntelManager', () => new PlayerIntelManager());
+	container.set('SpawnManager', () => new SpawnManager());
 }
