059f26fece231221ce1d13473ef249751f828e65
David Franke <mirroar@gmail.com>
2023-08-16 16:22:25 +0200

    Clean up resources report.

--- src/report/resources.ts
+++ src/report/resources.ts
@@ -7,8 +7,9 @@ declare global {
 }
 
 interface ResourceEntry {
-	color: string;
 	resourceType: ResourceConstant;
+	label?: string;
+	color: string;
 	height?: number;
 	scale?: number;
 	hidden?: boolean;
@@ -21,9 +22,9 @@ interface ResourceRow {
 
 const resourcesToReport: ResourceRow[] = [
 	{
-		label: 'carry',
+		label: '📦',
 		resources: [
-			{color: '#32167E', resourceType: RESOURCE_KEANIUM_BAR, height: 2, scale: 2 / 5},
+			{color: '#32167E', resourceType: RESOURCE_KEANIUM_BAR, height: 2, scale: 2 / 5, label: 'K🪨'},
 			{color: '#9973F7', resourceType: RESOURCE_KEANIUM, height: 2, scale: 2},
 			{color: '#9973F7', resourceType: RESOURCE_KEANIUM_HYDRIDE},
 			{color: '#9973F7', resourceType: RESOURCE_KEANIUM_ACID},
@@ -31,9 +32,9 @@ const resourcesToReport: ResourceRow[] = [
 		],
 	},
 	{
-		label: 'rangedAttack',
+		label: '🏹',
 		resources: [
-			{color: '#32167E', resourceType: RESOURCE_KEANIUM_BAR, hidden: true, scale: 2 / 5},
+			{color: '#32167E', resourceType: RESOURCE_KEANIUM_BAR, hidden: true, scale: 2 / 5, label: 'K🪨'},
 			{color: '#9973F7', resourceType: RESOURCE_KEANIUM, hidden: true, scale: 2},
 			{color: '#9973F7', resourceType: RESOURCE_KEANIUM_OXIDE},
 			{color: '#9973F7', resourceType: RESOURCE_KEANIUM_ALKALIDE},
@@ -41,9 +42,9 @@ const resourcesToReport: ResourceRow[] = [
 		],
 	},
 	{
-		label: 'build',
+		label: '🏗️',
 		resources: [
-			{color: '#346046', resourceType: RESOURCE_LEMERGIUM_BAR, height: 2, scale: 2 / 5},
+			{color: '#346046', resourceType: RESOURCE_LEMERGIUM_BAR, height: 2, scale: 2 / 5, label: 'L🪨'},
 			{color: '#70F0A9', resourceType: RESOURCE_LEMERGIUM, height: 2, scale: 2},
 			{color: '#70F0A9', resourceType: RESOURCE_LEMERGIUM_HYDRIDE},
 			{color: '#70F0A9', resourceType: RESOURCE_LEMERGIUM_ACID},
@@ -51,9 +52,9 @@ const resourcesToReport: ResourceRow[] = [
 		],
 	},
 	{
-		label: 'heal',
+		label: '💊',
 		resources: [
-			{color: '#346046', resourceType: RESOURCE_LEMERGIUM_BAR, hidden: true, scale: 2 / 5},
+			{color: '#346046', resourceType: RESOURCE_LEMERGIUM_BAR, hidden: true, scale: 2 / 5, label: 'L🪨'},
 			{color: '#70F0A9', resourceType: RESOURCE_LEMERGIUM, hidden: true, scale: 2},
 			{color: '#70F0A9', resourceType: RESOURCE_LEMERGIUM_OXIDE},
 			{color: '#70F0A9', resourceType: RESOURCE_LEMERGIUM_ALKALIDE},
@@ -61,9 +62,9 @@ const resourcesToReport: ResourceRow[] = [
 		],
 	},
 	{
-		label: 'attack',
+		label: '⚔️',
 		resources: [
-			{color: '#285F7E', resourceType: RESOURCE_UTRIUM_BAR, height: 2, scale: 2 / 5},
+			{color: '#285F7E', resourceType: RESOURCE_UTRIUM_BAR, height: 2, scale: 2 / 5, label: 'U🪨'},
 			{color: '#78D4F5', resourceType: RESOURCE_UTRIUM, height: 2, scale: 2},
 			{color: '#78D4F5', resourceType: RESOURCE_UTRIUM_HYDRIDE},
 			{color: '#78D4F5', resourceType: RESOURCE_UTRIUM_ACID},
@@ -71,9 +72,9 @@ const resourcesToReport: ResourceRow[] = [
 		],
 	},
 	{
-		label: 'harvest',
+		label: '⛏',
 		resources: [
-			{color: '#285F7E', resourceType: RESOURCE_UTRIUM_BAR, hidden: true, scale: 2 / 5},
+			{color: '#285F7E', resourceType: RESOURCE_UTRIUM_BAR, hidden: true, scale: 2 / 5, label: 'U🪨'},
 			{color: '#78D4F5', resourceType: RESOURCE_UTRIUM, hidden: true, scale: 2},
 			{color: '#78D4F5', resourceType: RESOURCE_UTRIUM_OXIDE},
 			{color: '#78D4F5', resourceType: RESOURCE_UTRIUM_ALKALIDE},
@@ -81,9 +82,9 @@ const resourcesToReport: ResourceRow[] = [
 		],
 	},
 	{
-		label: 'dismantle',
+		label: '🧨',
 		resources: [
-			{color: '#5A4D32', resourceType: RESOURCE_ZYNTHIUM_BAR, height: 2, scale: 2 / 5},
+			{color: '#5A4D32', resourceType: RESOURCE_ZYNTHIUM_BAR, height: 2, scale: 2 / 5, label: 'Z🪨'},
 			{color: '#F6D592', resourceType: RESOURCE_ZYNTHIUM, height: 2, scale: 2},
 			{color: '#F6D592', resourceType: RESOURCE_ZYNTHIUM_HYDRIDE},
 			{color: '#F6D592', resourceType: RESOURCE_ZYNTHIUM_ACID},
@@ -91,9 +92,9 @@ const resourcesToReport: ResourceRow[] = [
 		],
 	},
 	{
-		label: 'move',
+		label: '🚲',
 		resources: [
-			{color: '#5A4D32', resourceType: RESOURCE_ZYNTHIUM_BAR, hidden: true, scale: 2 / 5},
+			{color: '#5A4D32', resourceType: RESOURCE_ZYNTHIUM_BAR, hidden: true, scale: 2 / 5, label: 'Z🪨'},
 			{color: '#F6D592', resourceType: RESOURCE_ZYNTHIUM, hidden: true, scale: 2},
 			{color: '#F6D592', resourceType: RESOURCE_ZYNTHIUM_OXIDE},
 			{color: '#F6D592', resourceType: RESOURCE_ZYNTHIUM_ALKALIDE},
@@ -101,30 +102,32 @@ const resourcesToReport: ResourceRow[] = [
 		],
 	},
 	{
-		label: 'upgrade',
+		label: '🙏',
 		resources: [
-			{color: '#B4B4B4', resourceType: RESOURCE_GHODIUM_MELT, height: 2, scale: 2 / 5},
+			{color: '#B4B4B4', resourceType: RESOURCE_GHODIUM_MELT, height: 2, scale: 2 / 5, label: 'G🪨'},
 			{color: '#FFFFFF', resourceType: RESOURCE_GHODIUM, height: 2, scale: 2},
 			{color: '#FFFFFF', resourceType: RESOURCE_GHODIUM_HYDRIDE},
 			{color: '#FFFFFF', resourceType: RESOURCE_GHODIUM_ACID},
 			{color: '#FFFFFF', resourceType: RESOURCE_CATALYZED_GHODIUM_ACID},
+			{color: '#B4B4B4', resourceType: 'ZK'},
 		],
 	},
 	{
-		label: 'tough',
+		label: '🛡',
 		resources: [
-			{color: '#B4B4B4', resourceType: RESOURCE_GHODIUM_MELT, hidden: true, scale: 2 / 5},
+			{color: '#B4B4B4', resourceType: RESOURCE_GHODIUM_MELT, hidden: true, scale: 2 / 5, label: 'G🪨'},
 			{color: '#FFFFFF', resourceType: RESOURCE_GHODIUM, hidden: true, scale: 2},
 			{color: '#FFFFFF', resourceType: RESOURCE_GHODIUM_OXIDE},
 			{color: '#FFFFFF', resourceType: RESOURCE_GHODIUM_ALKALIDE},
 			{color: '#FFFFFF', resourceType: RESOURCE_CATALYZED_GHODIUM_ALKALIDE},
+			{color: '#B4B4B4', resourceType: 'UL'},
 		],
 	},
 	{
 		label: 'O',
 		resources: [
 			{color: '#B4B4B4', resourceType: RESOURCE_HYDROXIDE, height: 2, scale: 2},
-			{color: '#4C4C4C', resourceType: RESOURCE_OXIDANT, scale: 1 / 5},
+			{color: '#4C4C4C', resourceType: RESOURCE_OXIDANT, scale: 1 / 5, label: 'O🪨'},
 			{color: '#B4B4B4', resourceType: RESOURCE_OXYGEN},
 		],
 	},
@@ -132,14 +135,14 @@ const resourcesToReport: ResourceRow[] = [
 		label: 'H',
 		resources: [
 			{color: '#B4B4B4', resourceType: RESOURCE_HYDROXIDE, hidden: true, scale: 2},
-			{color: '#4C4C4C', resourceType: RESOURCE_REDUCTANT, scale: 1 / 5},
+			{color: '#4C4C4C', resourceType: RESOURCE_REDUCTANT, scale: 1 / 5, label: 'H🪨'},
 			{color: '#B4B4B4', resourceType: RESOURCE_HYDROGEN},
 		],
 	},
 	{
 		label: 'X',
 		resources: [
-			{color: '#532523', resourceType: RESOURCE_PURIFIER, scale: 1 / 5},
+			{color: '#532523', resourceType: RESOURCE_PURIFIER, scale: 1 / 5, label: 'X🪨'},
 			{color: '#EE837E', resourceType: RESOURCE_CATALYST},
 		],
 	},
@@ -154,8 +157,8 @@ export default class ResourcesReport {
 			align: 'left',
 		});
 
-		const minX = 5;
-		const maxX = 25;
+		const minX = 3;
+		const maxX = 30;
 		const maxResources = this.getHighestResourceAmountOfRow();
 		if (maxResources === 0) return;
 		const scale = (maxX - minX) / maxResources;
@@ -164,19 +167,25 @@ export default class ResourcesReport {
 			const resourceRow = resourcesToReport[i];
 			const y = i + 30;
 
-			visual.text(resourceRow.label, 1, y, {align: 'left'});
+			visual.text(resourceRow.label, 1, y + 0.3, {align: 'left'});
 
 			let currentX = minX;
 			for (const resourceInfo of resourceRow.resources) {
 				const width = this.getResourceAmount(resourceInfo.resourceType) / (resourceInfo.scale ?? 1) * scale;
+				const height = resourceInfo.height ?? 1;
 
 				if (!resourceInfo.hidden) {
-					visual.rect(currentX - 0.5, y - 0.5, width, resourceInfo.height ?? 1, {
+					visual.rect(currentX - 0.5, y - 0.5, width, height, {
 						fill: resourceInfo.color,
 						opacity: 0.8,
 						stroke: '#ffffff',
 						strokeWidth: 0.1,
 					});
+
+					const label = resourceInfo.label ?? resourceInfo.resourceType;
+					if (width > label.length / 2) {
+						visual.text(label, currentX - 0.5 + width / 2, y - 0.2 + height / 2);
+					}
 				}
 
 				currentX += width;
