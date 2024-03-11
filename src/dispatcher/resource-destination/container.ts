55dade257f8e0cbf77f3b31b41f7b7a0fb0d104f
David Franke <mirroar@gmail.com>
2024-03-11 17:40:39 +0100

    Let transporters deliver more energy to controller container, taking into account the approximate amount of energy that will be spent by upgraders while they travel.

--- src/dispatcher/resource-destination/container.ts
+++ src/dispatcher/resource-destination/container.ts
@@ -1,4 +1,6 @@
+import cache from 'utils/cache';
 import StructureDestination from 'dispatcher/resource-destination/structure';
+import utilities from 'utilities';
 
 interface ContainerDestinationTask extends StructureDestinationTask {
 	type: 'container';
@@ -33,28 +35,65 @@ export default class ContainerDestination extends StructureDestination<Container
 		const container = Game.getObjectById<StructureContainer>(this.room.memory.controllerContainer);
 		if (!container) return;
 
+		// @todo Take into account boosts.
+		const upgraderWorkParts = _.sum(this.room.creepsByRole.upgrader, creep => creep.getActiveBodyparts(WORK));
+		const refillPathLength = cache.inHeap('ccRefillPathLength:' + this.room.name, 5000, () => {
+			const path = utilities.getPath(this.room.getStorageLocation(), container.pos, false, {singleRoom: this.room.name});
+
+			if (path.incomplete) return 1;
+			return path.path.length;
+		});
+
+		const usedEnergyUntilArrival = upgraderWorkParts * UPGRADE_CONTROLLER_POWER * refillPathLength;
+		const otherDeliveringCreeps = this.room.getCreepsWithOrder(this.getType(), container.id);
+		const totalNeededEnergy = container.store.getFreeCapacity() + usedEnergyUntilArrival - _.sum(otherDeliveringCreeps, c => c.store.getUsedCapacity(RESOURCE_ENERGY));
+
 		const option: ContainerDestinationTask = {
 			priority: 4,
-			weight: container.store.getFreeCapacity() / 100,
+			weight: totalNeededEnergy / 100,
 			type: this.getType(),
 			target: container.id,
 			resourceType: RESOURCE_ENERGY,
-			amount: container.store.getFreeCapacity(),
+			amount: totalNeededEnergy,
 		};
 
 		let prioFactor = 1;
-		if (container.store.getUsedCapacity() / container.store.getCapacity() > 0.75) {
+		if (totalNeededEnergy / container.store.getCapacity() < 0.75) {
 			prioFactor = 3;
 			option.priority--;
 			option.priority--;
 		}
-		else if (container.store.getUsedCapacity() / container.store.getCapacity() > 0.5) {
+		else if (totalNeededEnergy / container.store.getCapacity() < 0.5) {
 			option.priority--;
 			prioFactor = 2;
 		}
 
-		option.priority -= this.room.getCreepsWithOrder(this.getType(), container.id).length * prioFactor;
-
 		options.push(option);
+	}
+
+	isValid(task: ContainerDestinationTask, context: ResourceDestinationContext) {
+		// We need an adjusted `isValid` check because even if the container is full, the task might still be valid.
+		const structure = Game.getObjectById(task.target);
+		if (!structure) return false;
+		if (context.creep.memory.singleRoom && structure.pos.roomName !== context.creep.memory.singleRoom) return false;
+		if (context.creep.store.getUsedCapacity(task.resourceType) === 0) return false;
+
+		// Full container only invalidates this task if we're already there.
+		if (structure.store.getFreeCapacity(task.resourceType) === 0 && context.creep.pos.getRangeTo(structure.pos) === 1) return false;
+
+		// @todo Take into account boosts.
+		const upgraderWorkParts = _.sum(this.room.creepsByRole.upgrader, creep => creep.getActiveBodyparts(WORK));
+		const refillPathLength = context.creep.pos.getRangeTo(structure.pos);
+		const usedEnergyUntilArrival = upgraderWorkParts * UPGRADE_CONTROLLER_POWER * refillPathLength;
+		const otherDeliveringCreeps = this.room.getCreepsWithOrder(this.getType(), task.target);
+		const totalNeededEnergy = structure.store.getFreeCapacity() + usedEnergyUntilArrival - _.sum(otherDeliveringCreeps, c => {
+			if (c.id === context.creep.id) return 0;
+			if (c.pos.getRangeTo(structure.pos) > refillPathLength) return 0;
+
+			return c.store.getUsedCapacity(RESOURCE_ENERGY);
+		});
+		if (totalNeededEnergy <= 0) return false;
+
+		return true;
 	}
 }
