1d62b6ff7f475804d1021bb371af131ac4cb6db7
David Franke <mirroar@gmail.com>
2024-04-26 12:01:47 +0200

    Track lab usage for rooms report.

--- src/process/rooms/owned/labs.ts
+++ src/process/rooms/owned/labs.ts
@@ -3,6 +3,17 @@ import ReactionsProcess from 'process/rooms/owned/labs.reactions';
 import PositionsProcess from 'process/rooms/owned/labs.position';
 import hivemind, {PROCESS_PRIORITY_LOW} from 'hivemind';
 
+declare global {
+	interface RoomMemory {
+		labUsage: {
+			busy: number;
+			idle: number;
+			waiting: number;
+			total: number;
+		}
+	}
+}
+
 export default class ManageLabsProcess extends Process {
 	room: Room;
 
@@ -45,24 +56,55 @@ export default class ManageLabsProcess extends Process {
 
 	runReactions() {
 		const memory = this.room.memory;
-		if (!memory.currentReaction) return;
 
 		const source1 = Game.getObjectById<StructureLab>(memory.labs.source1);
 		const source2 = Game.getObjectById<StructureLab>(memory.labs.source2);
 		if (!source1 || !source2) return;
-		if (source1.mineralType !== memory.currentReaction[0] || source2.mineralType !== memory.currentReaction[1]) return;
+
+		if (!memory.labUsage) memory.labUsage = {
+			busy: 0,
+			idle: 0,
+			waiting: 0,
+			total: 0,
+		};
+
+		memory.labUsage.total++;
+		if (memory.labUsage.total >= 10_000) {
+			memory.labUsage.busy /= 2;
+			memory.labUsage.idle /= 2;
+			memory.labUsage.waiting /= 2;
+			memory.labUsage.total /= 2;
+		}
+
+		if (!memory.currentReaction) {
+			memory.labUsage.idle++;
+			return;
+		}
+		if (source1.mineralType !== memory.currentReaction[0] || source2.mineralType !== memory.currentReaction[1]) {
+			memory.labUsage.waiting++;
+			return;
+		}
 
 		const labs = memory.labs.reactor;
-		if (!labs) return;
+		if (!labs || labs.length === 0) {
+			memory.labUsage.waiting++;
+			return;
+		}
 
+		let totalLabs = 0;
+		let busyLabs = 0;
 		for (const reactorID of labs) {
 			if (this.room.boostManager.isLabUsedForBoosting(reactorID)) continue;
 
+			totalLabs++;
 			const reactor = Game.getObjectById<StructureLab>(reactorID);
-
-			if (reactor && reactor.cooldown <= 0) {
-				reactor.runReaction(source1, source2);
+			if (reactor) {
+				busyLabs++;
+				if (!reactor.cooldown && reactor.runReaction(source1, source2) !== OK) busyLabs--;
 			}
 		}
+
+		memory.labUsage.busy += busyLabs / totalLabs;
+		memory.labUsage.waiting += (totalLabs - busyLabs) / totalLabs;
 	}
 }
