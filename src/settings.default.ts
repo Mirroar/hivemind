9e4913b115ac81b335974204e7d91a47db7c5262
Mirroar <mirroar@gmail.com>
2025-10-15 20:51:56 +0200

    feat: Don't abandon rooms unless we have more than configured minimum amount.

--- src/settings.default.ts
+++ src/settings.default.ts
@@ -109,6 +109,7 @@ const settings: SettingsObject = {
 	// Expansion:
 	// Number of ticks we may cache expansion scores.
 	expansionScoreCacheDuration: 20_000,
+	minOwnedRooms: 1,
 	maxOwnedRooms: null,
 	// Maximum amount of CPU that may be spent each tick on finding a suitable
 	// expansion target.
